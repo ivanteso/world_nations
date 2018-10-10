76f8dd37f740f83cf4ecf8c5c0091cd9a2cd342e
ivan_teso <ivan.teso@gmail.com>
2018-10-10 18:15:32 +0300

    Feat: add stack navigator
    Move the whole HomeScreen in components folder

--- App.js
+++ App.js
@@ -4,65 +4,17 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, FlatList, Image} from 'react-native';
+import {createStackNavigator} from 'react-navigation';
+import HomeScreen from './components/HomeScreen'
 
-export default class App extends React.Component {
 
-  state = {
-    nations: [],
-  }
-
-  componentDidMount() {
-    fetch('https://restcountries.eu/rest/v2/all')
-      .then(res => res.json())
-      .then(json => {
-        this.setState({ nations: json });
-      });
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>World Nations</Text>
-        <FlatList
-          data={ this.state.nations}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={arg =>
-            <View>
-              <Text style={styles.nations}>{arg.item.name}</Text>
-              {
-                arg.item.borders.map(border => {
-                  return <Text style={styles.text}>{border}</Text>
-                })
-              }
-
-            </View>
-          }
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+export default createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    }
   },
-  welcome: {
-    fontSize:30,
-    color:'darkgray',
-    textTransform: 'uppercase',
-    textAlign:'center',
-    letterSpacing:1,
-    margin:30,
-  },
-  nations: {
-    fontSize:60,
-    alignSelf:'center',
-  },
-  text: {
-    fontSize: 18,
-    alignSelf:'center',
+  {
+    initialRouteName: 'Home',
   }
-});
+);
