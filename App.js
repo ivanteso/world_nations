84e31567dfdb2f2e443e3b86a89164ae4bf405b5
ivan_teso <ivan.teso@gmail.com>
2018-10-10 18:00:01 +0300

    First commit World Nations

---
+++ App.js
@@ -0,0 +1,68 @@
+/**
+ World Countries app
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, FlatList, Image} from 'react-native';
+
+export default class App extends React.Component {
+
+  state = {
+    nations: [],
+  }
+
+  componentDidMount() {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ nations: json });
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>World Nations</Text>
+        <FlatList
+          data={ this.state.nations}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={arg =>
+            <View>
+              <Text style={styles.nations}>{arg.item.name}</Text>
+              {
+                arg.item.borders.map(border => {
+                  return <Text style={styles.text}>{border}</Text>
+                })
+              }
+
+            </View>
+          }
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcome: {
+    fontSize:30,
+    color:'darkgray',
+    textTransform: 'uppercase',
+    textAlign:'center',
+    letterSpacing:1,
+    margin:30,
+  },
+  nations: {
+    fontSize:60,
+    alignSelf:'center',
+  },
+  text: {
+    fontSize: 18,
+    alignSelf:'center',
+  }
+});
